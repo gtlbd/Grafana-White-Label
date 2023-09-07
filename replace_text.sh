3010d623460742d9cd1b26e9c58b7899ada6f009
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-07 16:32:58 +0600

    Loading3

--- replace_text.sh
+++ replace_text.sh
@@ -7,20 +7,10 @@ source_Loading_text="Loading Grafana"
 replacement_Loading_text="Loading Enerzyz"
 
 # Specify the parent directory where the subdirectory containing files is located
-parent_directory="/usr/share/grafana/public/"
+parent_directory="/usr/share/grafana/public/build/"
 
-# Iterate through all files in the subdirectory
-for subdirectory in "$parent_directory"*
-do
-    if [ -d "$subdirectory" ]; then
-        for file in "$subdirectory"*
-        do
-            if [ -f "$file" ]; then
-                # Use sed to replace the text and save it back to the file
-                sed -i "s/$source_Welcome_text/$replacement_Welcome_text/g" "$file"
-                sed -i "s/$source_Loading_text/$replacement_Loading_text/g" "$file"
-                echo "Replaced in $file"
-            fi
-        done
-    fi
-done
+# Find and replace text recursively in all files under the parent directory
+find "$parent_directory" -type f -exec sed -i "s/$source_Welcome_text/$replacement_Welcome_text/g" {} \;
+find "$parent_directory" -type f -exec sed -i "s/$source_Loading_text/$replacement_Loading_text/g" {} \;
+
+echo "Text replacement completed."
