11cf8405a6dc1e5f9957b1201c38276dec3ccc1f
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-07 13:23:55 +0600

    fav32.svg

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 sudo mkdir -p /home/ramim/Grafana-White-Label/Original_Files/public
 sudo cp -ru /usr/share/grafana/public/* /home/$USER/Grafana-White-Label/Original_Files/public/
 sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/grafana_icon.svg /usr/share/grafana/public/img/
-echo -e "Done"
+echo -e "Done - grafana_icon.svg"
+sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/fav32.svg /usr/share/grafana/public/img/
+echo -e "Done - fav32.svg"
