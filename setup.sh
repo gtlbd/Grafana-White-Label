acd3c15af3731ee8e44ed65256e73341243db25e
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-07 12:29:09 +0600

    setup.sh5

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /home/ramim/Grafana-White-Label/Original_Files/public
-sudo cp /usr/share/grafana/public/* /home/$USER/Grafana-White-Label/Original_Files/public/*
+sudo mkdir -p /home/ramim/Grafana-White-Label/Original_Files/public
+sudo cp /usr/share/grafana/public/* /home/$USER/Grafana-White-Label/Original_Files/public/
 sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/grafana_icon.svg /usr/share/grafana/public/img/
