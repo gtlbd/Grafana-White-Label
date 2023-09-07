ca29048c49f0dd5434b52bc723e339b1a8d97a3b
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-07 17:54:54 +0600

    Loading5

--- replace_text.sh
+++ replace_text.sh
@@ -7,20 +7,40 @@ source_Loading_text="Loading Grafana"
 replacement_Loading_text="Loading Enerzyz"
 
 # Specify the parent directory where the subdirectory containing files is located
-parent_directory="/usr/share/grafana/public/"
-
+#parent_directory="/usr/share/grafana/public/build/"
+# Iterate through all files in the directory
+directory="/usr/share/grafana/public/build/"
+for file in "$directory"*
+do
+    if [ -f "$file" ]; then
+        # Use sed to replace the text and save it back to the file
+        sed -i "s/$source_Welcome_text/$replacement_Welcome_text/g" "$file"
+        echo "Replaced in $file"
+    fi
+done
+echo "Text replacement completed. for - Welcome"
+directory="/usr/share/grafana/public/views/"
+for file in "$directory"*
+do
+    if [ -f "$file" ]; then
+        # Use sed to replace the text and save it back to the file
+        sed -i "s/$source_Loading_text/$replacement_Loading_text/g" "$file"
+        echo "Replaced in $file"
+    fi
+done
+echo "Text replacement completed. for - Loading"
 # Find and replace text recursively in all files under the parent directory
-find "$parent_directory" -type f -exec sh -c '
-    for file; do
-        sed -i "s/$0/$1/g" "$file"
-        echo "Replaced in $file"
-    done
-' "$source_Welcome_text" "$replacement_Welcome_text" {} +
-find "$parent_directory" -type f -exec sh -c '
-    for file; do
-        sed -i "s/$0/$1/g" "$file"
-        echo "Replaced in $file"
-    done
-' "$source_Loading_text" "$replacement_Loading_text" {} +
+# find "$parent_directory" -type f -exec sh -c '
+    # for file; do
+        # sed -i "s/$0/$1/g" "$file"
+        # echo "Replaced in $file"
+    # done
+# ' "$source_Welcome_text" "$replacement_Welcome_text" {} +
+# find "$parent_directory" -type f -exec sh -c '
+    # for file; do
+        # sed -i "s/$0/$1/g" "$file"
+        # echo "Replaced in $file"
+    # done
+# ' "$source_Loading_text" "$replacement_Loading_text" {} +
 
-echo "Text replacement completed."
+#echo "Text replacement completed. for - Loading"
