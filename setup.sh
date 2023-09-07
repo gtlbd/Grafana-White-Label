5597deb35f0aa5966cb4f7865e120b0d61181010
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-07 13:35:24 +0600

    fav32.png

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo mkdir -p /home/ramim/Grafana-White-Label/Original_Files/public
+echo -e "Starting Backup - from: '/usr/share/grafana/public/', to: '/home/$USER/Grafana-White-Label/Original_Files/public/'"
 sudo cp -ru /usr/share/grafana/public/* /home/$USER/Grafana-White-Label/Original_Files/public/
+echo -e "Done - Backup"
 sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/grafana_icon.svg /usr/share/grafana/public/img/
 echo -e "Done - grafana_icon.svg"
-sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/fav32.svg /usr/share/grafana/public/img/
-echo -e "Done - fav32.svg"
+sudo scp /home/$USER/Grafana-White-Label/grafana/public/img/fav32.png /usr/share/grafana/public/img/
+echo -e "Done - fav32.png"
