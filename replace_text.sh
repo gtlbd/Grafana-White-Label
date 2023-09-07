d287ed4462e7256f0340f600b3eb3299316ffcd7
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-08 01:34:57 +0600

    AppTitle4

--- replace_text.sh
+++ replace_text.sh
@@ -7,7 +7,7 @@ source_Loading_text="Loading Grafana"
 replacement_Loading_text="Loading Enerzyz"
 source_AppTitle_text='AppTitle="Grafana"'
 replacement_AppTitle_text='AppTitle="Enerzyz"'
-source_AppTitle2_text="<title>[[.AppTitle]]</title>"
+source_AppTitle2_text="<title>\[\[\.AppTitle\]\]</title>"
 replacement_AppTitle2_text="<title>Enerzyz</title>"
 
 # Specify the parent directory where the subdirectory containing files is located
@@ -30,7 +30,7 @@ do
     if [ -f "$file" ]; then
         # Use sed to replace the text and save it back to the file
         sed -i "s/$source_Loading_text/$replacement_Loading_text/g" "$file"
-		sed -i "s/$source_AppTitle2_text/$replacement_AppTitle2_text/g" "$file"
+		sed -Ei "s/$source_AppTitle2_text/$replacement_AppTitle2_text/g" "$file"
         echo "Replaced in $file"
     fi
 done
