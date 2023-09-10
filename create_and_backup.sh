a6672b0c8fd89fc8842976e1f4f6922f92a3ba6d
Sakiul Mowla Ramim <romim420@gmail.com>
2023-09-10 11:35:00 +0600

    restore_default.sh6

--- create_and_backup.sh
+++ create_and_backup.sh
@@ -4,35 +4,12 @@
 source_dir="/usr/share/grafana/public/"
 destination_dir="/home/$USER/Grafana-White-Label/Original_Files/public/"
 
-# Function to copy files from source to destination if they don't exist in the destination
-function copy_if_not_exist {
-    source_file="$1"
-    destination_file="$2"
-    
-    # Check if the destination file exists
-    if [ ! -e "$destination_file" ]; then
-        echo "Copying '$source_file' to '$destination_file'"
-        sudo cp "$source_file" "$destination_file"
-    fi
-}
+# Create the backup directory if it doesn't exist
+mkdir -p "$destination_dir"
 
-# Check if the destination directory exists, create it if not
-if [ ! -d "$destination_dir" ]; then
-    echo "Creating destination directory: '$destination_dir'"
-    sudo mkdir -p "$destination_dir"
-fi
+echo -e "Starting Backup - from: '$source_dir', to: '$destination_dir'"
 
-echo "Starting Backup - from: '$source_dir', to: '$destination_dir'"
+# Use rsync to create a backup of files and directories recursively
+sudo rsync -av --ignore-existing "$source_dir" "$destination_dir"
 
-# Iterate over files in the source directory and copy if they don't exist in the destination
-for file in "$source_dir"*
-do
-    if [ -f "$file" ]; then
-        file_name=$(basename "$file")
-        destination_file="$destination_dir$file_name"
-        
-        copy_if_not_exist "$file" "$destination_file"
-    fi
-done
-
-echo "Backup completed"
+echo -e "Backup completed"
